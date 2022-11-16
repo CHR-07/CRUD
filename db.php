f6366c2bd005eeecd48205ae55b125a9a5d78f0f
CHR-07 <alberto_chr@hotmail.com>
2022-11-15 21:01:14 -0600

    first commit

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+$conn = mysqli_connect(
+  'sql201.epizy.com',
+  'epiz_33010582',
+  'URSI6Ww0QhU',
+  'epiz_33010582_php_mysql_crud'
+) or die(mysqli_erro($mysqli));
+
+?>
